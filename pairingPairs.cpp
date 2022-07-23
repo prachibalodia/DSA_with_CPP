9aa4ec93d585e1f08ceee56938b3b769c697f4f0
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2022-07-23 17:03:58 +0530

    Add files via upload

---
+++ pairingPairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void printPairs(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int x= arr[i];
+        for(int j=i+1;j<n;j++)
+        {
+            int y=arr[j];
+
+            cout<< x<<", "<<y<<endl;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+ int arr[]= {1,2,3,4,5,6,7};
+ int n= sizeof(arr)/sizeof(int);
+
+ printPairs(arr,n);
+
+return 0;
+}
