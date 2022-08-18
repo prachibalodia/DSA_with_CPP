204c4d58b7e3e9189ad1747ed1a2281fc47b00da
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2022-08-19 00:41:06 +0530

    Add files via upload

---
+++ String_append.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string a= "prachi ";
+    string b="balodia";
+    a+=b;
+    cout<<a<<endl;
+char ch= '@';
+    a.append(1,ch);
+     cout<<a<<endl;
+
+    
+    
+ 
+return 0;
+}
