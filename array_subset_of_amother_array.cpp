facf1d229934abaff9045a2d1330c51e18d79ee0
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-01-15 21:09:46 +0530

    Add files via upload

--- array_subset_of_amother_array.cpp
+++ array_subset_of_amother_array.cpp
@@ -1,12 +1,20 @@
 string isSubset(int a1[], int a2[], int n, int m) {
     
-      map<int,int> mp;
-    for(int i = 0; i< n; i++) mp[a1[i]]++;
-    for(int i =0; i<m; i++)
-    {
-        if(mp.count(a2[i]) && mp[a2[i]] != 0) mp[a2[i]]--;
-        else if( mp[a2[i]] == 0) return "No";
+    if(n<m){
+        return "No";
+    }
+    map<int, int> mp;
+    for(int i=0;i<n;i++){
+        mp[a1[i]]++;
+    }
+
+    for(int i=0;i<m;i++){
+        if(mp.count(a2[i]) && mp[a2[i]] != 0){
+            mp[a2[i]]--;
+        }
+        else{
+            return "No";
+        }
     }
     return "Yes";
-   
 }
