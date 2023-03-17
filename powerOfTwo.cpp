7a176f957c155da1404aeb3f7f06db7bacee2f3e
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-03-17 20:04:02 +0530

    Add files via upload

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int n;
-    cin>>n;
-    if((n & (n-1)) == 0){
-        cout<< "Yes, a power of two";
+class Solution {
+public:
+
+    //using bit manipulation
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        return((n & (n-1))==0);
     }
-    else{
-        cout<<" No";
-    }
- 
-return 0;
-}
+
+    //using recursion
+    // bool isPowerOfTwo(int n) {
+    //     if(n==0) return false;
+    //     //base case
+    //     if(n==1) return n;
+
+    //     return (n%2==0 && isPowerOfTwo(n/2));
+    // }
+};
