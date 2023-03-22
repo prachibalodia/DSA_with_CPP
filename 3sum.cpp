680476a70f444c7546df2574ccd2787b4a34922c
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-03-22 21:26:51 +0530

    Add files via upload

---
+++ 3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        //two pointer approach
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        set<vector<int>> s;
+        for(int i=0;i<nums.size();i++){
+            int j=i+1;
+            int k= nums.size()-1;
+            while(j<k){
+                   if(nums[i]+nums[j]+nums[k] == 0){
+                s.insert({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
+            }
+            else if(nums[i]+nums[j]+nums[k] >0){
+                k--;
+            }
+            else{
+                j++;
+            }
+            }
+        }
+
+        for(auto x: s)
+            result.push_back(x);
+         return result;
+    }
+};
