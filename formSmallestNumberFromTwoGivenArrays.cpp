c0daf8660d4df55dce3145e743537e343b41b541
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-04-01 20:35:45 +0530

    Add files via upload

---
+++ formSmallestNumberFromTwoGivenArrays.cpp
@@ -0,0 +1,27 @@
+//LC contest qs
+class Solution {
+public:
+    int minNumber(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        map<int, int> m;
+        for(int i=0;i< nums1.size();i++){
+            m[nums1[i]]++;
+        }
+         for(int i=0;i< nums2.size();i++){
+            m[nums2[i]]++;
+        }
+        for(auto i: m){
+            if(i.second > 1){
+                return i.first;
+            }
+        }
+        if(nums1[0]<nums2[0]){
+            return (nums1[0]*10) + nums2[0];
+        }
+        else{
+            return (nums2[0]*10) + nums1[0];
+        }
+
+    }
+};
