ad5d23ea058073affde850bf8cad66475e96ff14
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-04-27 21:52:53 +0530

    Add files via upload

---
+++ bulbSwitcher.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        //test cases fails
+        // int sum=0, c=0;
+        // for(int i=1;i<=n;i=i+2){
+        //     sum+=i;
+        //     if(sum>i){
+        //         break;
+        //     }
+        //     c++;
+        // }
+        // return c;
+        return sqrt(n);
+    }
+};
