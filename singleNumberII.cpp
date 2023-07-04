97e15bb35ee641e98828cf437d7119dd579e8562
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-07-05 00:01:32 +0530

    Add files via upload

--- singleNumberII.cpp
+++ singleNumberII.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>m;
-        for(auto i:nums){
-            m[i]++;
+
+        //brute force-> T= O(N)*2, S= O(N) 
+        // map<int,int>m;
+        // for(auto i:nums){
+        //     m[i]++;
+        // }
+        // for(auto i:m){
+        //     if(i.second == 1)
+        //     return i.first;
+        // }
+        // return -1;
+
+        //optimal-> 
+        //using bit manipulation
+        int n= nums.size();
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                if(((nums[j]>>i) & 1)==1){
+                    sum++;
+                    sum%=3;
+                }
+            }
+            if(sum!=0){
+                    ans|= sum<<i;
+            }
         }
-        for(auto i:m){
-            if(i.second == 1)
-            return i.first;
-        }
-        return -1;
+        return ans;        
     }
 };
