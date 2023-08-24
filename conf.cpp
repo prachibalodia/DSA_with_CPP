3da217703c52b8e3198db73eed6f6067c2e62d48
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-08-24 21:58:19 +0530

    Add files via upload

---
+++ conf.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ for(int i=0;i<108;i++){
+    cout<<"You deserve the world. You are going to be excellent tomorrow and ace it."<<endl;
+ }
+return 0;
+}
